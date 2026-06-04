021ba9ed32a3aa58aa5f53d62c30fdf3658cc927
Daniel Bork <daniel.bork@yahoo.com.br>
2026-06-04 18:15:26 -0300

    fix: adjust elements stripe

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,13 @@ import { Router } from '../src/routes/index'
 import { GlobalStyles } from './styles/globalStyles'
 import { ToastContainer } from 'react-toastify'
 import AppProvider from './hooks'
-import { Elements } from '@stripe/react-stripe-js'
-import stripePromise from './config/stripeConfig'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProvider>
-      <Elements stripe={stripePromise}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </Elements>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyles />
       <ToastContainer />
     </AppProvider>
